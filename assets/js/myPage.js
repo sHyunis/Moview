002167f1882e5fc90baf9d54624c622c2e44ebec
789elf <789elf@naver.com>
2024-08-02 16:37:00 +0900

    최근 본 영화 목록 마이페이지로 이동,  마이페이지 전체 css 수정

--- assets/js/myPage.js
+++ assets/js/myPage.js
@@ -2,20 +2,11 @@ const activity = document.querySelector('.activity');
 const activityHeader = document.querySelector('.activity-header');
 let parentList;
 
-function resetElement(targetElement) {
-    activity.removeChild(activity.lastChild);
 
-    document.querySelectorAll('.selected').forEach((element) => {
-        element.classList.remove('selected');
-    });
-
-    targetElement.classList.add('selected');
-}
-
-
-async function dummyRatingFetch(userId) {
-    // 대충 firebase 데이터 가져오는 코드 작성 (userId로 user가 작성한 평점 데이터 가져오기)
-    // 대충 데이터 가공해서 반환하기
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * 데이터 가져오기 (임시 더미) * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+async function fetchRating(userId) {
     const ratingDummy = [
         { title: "인터스텔라", backdrop_path: "/assets/img/test_img/인터스텔라.jpg", rating: 5 },
         { title: "나루토", backdrop_path: "/assets/img/test_img/나루토2.jpg", rating: 5 }
@@ -24,24 +15,7 @@ async function dummyRatingFetch(userId) {
 }
 
 
-// 리뷰와 좋아요의 html 구조는 거의 유사하기 때문에
-// 레뷰 데이터와 줄거리데이터(좋아요에 필요한 데이터)를 동일한 이름의 변수로 통일하면
-// 좋을 것 같아요. (html 생성 함수 하나로 통합 가능)
-// 혹은 html 생성하는 코드에서 처리해도 됩니다.
-// review와 preview(줄거리) 데이터를 적절하게 통합해서 사용하면 좋을 것 같아요.
-async function dummyReviewFetch(userId) {
-    /**
-     * 대충 firebase에 리뷰 데이터 가져오는 코드 작성 (userId 기반)
-     * 대충 이렇게 생긴 데이터? [{userId: qwer, movieId: 1234, review: "ㅋㅋㅋㅋㅋ"}]
-     * 
-     * 리뷰 데이터에 있는 movieId로 영화 데이터 api요청
-     * (근데 하나의 컬렉션에서 작업한다면 api 요청 필요 X)
-     * 예시(영화정보도 DB에 다 때려박기)
-     * [{userId: qwer, movieId: 1234, title: "하울", backdrop_path:"/assets/img/test_img/인터스텔라.jpg", review: "ㅋㅋㅋㅋㅋ"}]
-     * 
-     * */
-
-    // 아래 데이터에서 title, backdrop_path는 movie_id로 api 요청해서 가져와야 합니다?
+async function fetchReview(userId) {
     const reviewDummy = [
         { title: "인터스텔라", backdrop_path: "/assets/img/test_img/인터스텔라.jpg", content: "ㅋㅋ 쿠퍼 레전드" },
         { title: "하울", backdrop_path: "/assets/img/test_img/하울.jpg", content: "하울 너무 멋져요" },
@@ -52,7 +26,7 @@ async function dummyReviewFetch(userId) {
 }
 
 // 좋아요 더미 데이터
-async function dummyLikeFetch(userId) {
+async function fetchLike(userId) {
     const likeDummy = [
         { title: "인터스텔라", backdrop_path: "/assets/img/test_img/인터스텔라.jpg", content: "지구의 식량이 어쩌구 저쩌구.. 부족해져서 인류를 구원하기 위해 쿠퍼가 모험을 어쩌구 .." },
         { title: "조커", backdrop_path: "/assets/img/test_img/조커.jpg", content: "놀림 받고 흑화한 조커! 계단에서 춤춘다!" },
@@ -64,54 +38,117 @@ async function dummyLikeFetch(userId) {
         { title: "조커", backdrop_path: "/assets/img/test_img/조커.jpg", content: "놀림 받고 흑화한 조커! 계단에서 춤춘다!" },
         { title: "나루토", backdrop_path: "/assets/img/test_img/나루토.jpg", content: "대충 닌자 나오는 내용!" },
     ]
-
     return likeDummy;
 }
 
+// 최근 본 영화 목록
+async function fetchRecent() {
+    const tempRecent = JSON.parse(localStorage.getItem('recentMovies'));
+
+    // 중복 제거
+    const unique = new Set();
+    const recentMovies = tempRecent.filter(item => {
+        const duplicate = unique.has(item.id);
+        item.backdrop_path = `https://image.tmdb.org/t/p/w500${item.backdrop_path}`
+        item.rating = item.vote_average;
+        unique.add(item.id);
+        return !duplicate;
+    });
+
+    return recentMovies;
+}
+
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * 탭 전환 이벤트 처리  * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// 새로 탭 누르면 이전 탭 초기화
+function resetTab(targetElement) {
+    activity.removeChild(activity.lastChild);
+
+    document.querySelectorAll('.selected').forEach((element) => {
+        element.classList.remove('selected');
+    });
+
+    targetElement.classList.add('selected');
+}
+
+
+/**
+ * (탭 전환 이벤트)
+ * class 생성 규칙
+ * ${type}-list: 탭에 따라 class 생성 ex) rating-list, review-list ...  (당장은 안쓰는데 혹시 모르니..)
+ * common-list: 모든 탭에 공통으로 적용되는 css 클래스
+ * row-list: 평점, 최근 본목록은 flex-row 레이아웃
+ * column-list: 리뷰, 좋아요는 flex-column 레이아웃
+ */
 document.querySelector('.activity').addEventListener('click', async function (event) {
     const targetElement = event.target;
-    if (targetElement.closest('.rating-box')) {
-        resetElement(targetElement);
-        createParentList(["rating-list", "item-list"]);
+    if (targetElement.closest('.activity-tab')) {
+        const type = targetElement.textContent.toLowerCase(); // Rating, Review, Like, Recent
 
-        const data = await dummyRatingFetch("1234");
+        resetTab(targetElement);
+        if (type === 'rating' || 'recent') {
+            createParentList([`${type}-list`, "common-list", `row-list`]);
+        } else {
+            createParentList([`${type}-list`, "common-list", `column-list`]);
+        }
 
-        processRatingData(data);
-
-    } else if (targetElement.closest('.review-box')) {
-        resetElement(targetElement);
-
-        createParentList(["review", "common-list", "item-list"]);
-
-        const data = await dummyReviewFetch("1234");
-        processCommonData(data, "review");
-
-    } else if (targetElement.closest('.like-box')) {
-        resetElement(targetElement);
-
-        createParentList(["like", "common-list", "item-list"]);
-
-        const data = await dummyLikeFetch("1234");
-        processCommonData(data, "like");
-
+        fetchDataByType(type);
     }
 });
+// 탭 클릭시 분기 나누기
+async function fetchDataByType(type) {
+    let data = null;
+    let callback = null;
+
+    switch (type) {
+        case 'rating':
+            data = await fetchRating();
+            callback = processRowData;
+            break;
+        case 'recent':
+            data = await fetchRecent();
+            callback = processRowData;
+            break;
+        case 'review':
+            data = await fetchReview();
+            callback = processColumnData;
+            break;
+        case 'like':
+            data = await fetchLike();
+            callback = processColumnData;
+            break;
+        default:
+            throw new Error('이상한 타입');
+    }
+
+    callback(data, type);
+}
 
 
-function processRatingData(results) {
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * *  HTML 생성  * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+
+// 평점, 최근본
+const processRowData = (results, type) => {
     results.forEach((data) => {
-        renderRatingList(data);
+        renderRowList(data, type);
     })
 }
-function processCommonData(results, type) {
+
+// 리뷰, 좋아요
+const processColumnData = (results, type) => {
     results.forEach((data) => {
-        renderCommonList(data, type);
+        renderColumnList(data, type);
     })
 }
 
 
-
-// 반복하지 않는 부모 HTML 생성 
+// 부모 HTML 생성 
 function createParentList(classList) {
     parentList = document.createElement('ul');
 
@@ -122,17 +159,18 @@ function createParentList(classList) {
     activity.appendChild(parentList);
 }
 
-// 부모 HTML 하위에 반복해서 HMTL 생성 (별점)
-function renderRatingList(data) {
+
+// flex-row list 생성 (평점, 최근 본 영화 목록)
+function renderRowList(data, type) {
     const htmlContent = `
-        <li class="rating-container">
-            <div class="rating-img">
+        <li class="${type}-container row-container">
+            <div class="${type}-img row-img">
                 <a href="../index.html"> <img src="${data.backdrop_path}" alt="${data.title}"></a>
             </div>
-            <div class="rating-title">
+            <div class="${type}-title row-title">
                 <p>${data.title}</p>
             </div>
-            <div class="rating-rating">
+            <div class="${type}-rating row-rating">
                 ★ ${data.rating}
             </div>
         </li>
@@ -141,14 +179,14 @@ function renderRatingList(data) {
     parentList.innerHTML += htmlContent;
 }
 
-// 부모 HTML 하위에 반복해서 HMTL 생성 (리뷰, 좋아요 통합)
-function renderCommonList(data, type) {
+// flex-column list 생성 (리뷰, 좋아요)
+function renderColumnList(data, type) {
     const htmlContent = `
-        <li class="${type} common-container">
-            <div class="${type} common-img">
+        <li class="${type} column-container">
+            <div class="${type} column-img">
                 <a href="../index.html"><img src="${data.backdrop_path}"></a>
             </div>
-            <div class="${type} common-contents">
+            <div class="${type} column-contents">
                 <h4>${data.title}</h4>
                 <p>${data.content}</p>
                 <div class="feed-box">
@@ -156,10 +194,21 @@ function renderCommonList(data, type) {
                     <p>댓글(323)</p>
                 </div>
             </div>
-            <div class="${type} common-date">
+            <div class="${type} column-date">
                 <p>24분 전</p>
             </div>
         </li>
     `
     parentList.innerHTML += htmlContent;
 }
+
+
+
+(function () {
+    const ratingBox = document.querySelector('.rating-box p');
+    console.log(ratingBox);
+    ratingBox.classList.add('selected')
+
+    createParentList([`rating-list`, "common-list", `row-list`]);
+    fetchDataByType('rating');
+})()
