f7607889c5f8c1403b436b06f2769a932d54dd95
sHyunis <162306191+sHyunis@users.noreply.github.com>
2024-08-07 11:18:04 +0900

    수정

--- assets/js/myPage.js
+++ assets/js/myPage.js
@@ -1,131 +1,133 @@
 import { db, collection, getDocs, query, where } from "./fireBaseConfig.js";
 
-const activity = document.querySelector('.activity');
+const activity = document.querySelector(".activity");
 let parentList;
 
 async function fetchLike(userId) {
-    try {
-        const loginSession = sessionStorage.getItem("userLoginId");
-        const likeData = await getDocs(collection(db, "like"));
+  try {
+    const loginSession = sessionStorage.getItem("userLoginId");
+    const likeData = await getDocs(collection(db, "like"));
 
-        const promises = likeData.docs.map(async (item) => {
-            const data = item.data();
+    const promises = likeData.docs.map(async (item) => {
+      const data = item.data();
 
-            const q = query(
-                collection(db, 'like'),
-                where('movie_id', '==', data.movie_id),
-                where('like', '==', true)
-            );
+      const q = query(
+        collection(db, "like"),
+        where("movie_id", "==", data.movie_id),
+        where("like", "==", true)
+      );
 
-            const querySnapshot = await getDocs(q);
-            data.likeCount = querySnapshot.size;
+      const querySnapshot = await getDocs(q);
+      data.likeCount = querySnapshot.size;
 
-            if (data.user_id === loginSession && data.like === true) {
-                return data;
-            }
-            return null;
-        })
+      if (data.user_id === loginSession && data.like === true) {
+        return data;
+      }
+      return null;
+    });
 
-        const results = await Promise.all(promises);
+    const results = await Promise.all(promises);
 
-        return results.filter(data => data !== null).sort((a, b) => {
-            return new Date(b.movie_like_time) - new Date(a.movie_like_time);
-        });
-    } catch (e) {
-        console.error("fetchLike Error =>", e);
-    }
+    return results
+      .filter((data) => data !== null)
+      .sort((a, b) => {
+        return new Date(b.movie_like_time) - new Date(a.movie_like_time);
+      });
+  } catch (e) {
+    console.error("fetchLike Error =>", e);
+  }
 }
 
 async function fetchRecent() {
-    const tempRecent = JSON.parse(localStorage.getItem('recentMovies'));
+  const tempRecent = JSON.parse(localStorage.getItem("recentMovies"));
 
-    const unique = new Set();
-    const recentMovies = tempRecent.filter(item => {
-        const duplicate = unique.has(item.id);
-        item.backdrop_path = `https://image.tmdb.org/t/p/original${item.backdrop_path}`
-        item.rating = item.vote_average;
-        unique.add(item.id);
-        return !duplicate;
-    });
+  const unique = new Set();
+  const recentMovies = tempRecent.filter((item) => {
+    const duplicate = unique.has(item.id);
+    item.backdrop_path = `https://image.tmdb.org/t/p/original${item.backdrop_path}`;
+    item.rating = item.vote_average;
+    unique.add(item.id);
+    return !duplicate;
+  });
 
-    return recentMovies;
+  return recentMovies;
 }
 
 function resetTab(targetElement) {
-    activity.removeChild(activity.lastChild);
+  activity.removeChild(activity.lastChild);
 
-    document.querySelectorAll('.selected').forEach((element) => {
-        element.classList.remove('selected');
-    });
+  document.querySelectorAll(".selected").forEach((element) => {
+    element.classList.remove("selected");
+  });
 
-    targetElement.classList.add('selected');
+  targetElement.classList.add("selected");
 }
 
-document.querySelector('.activity').addEventListener('click', async function (event) {
+document
+  .querySelector(".activity")
+  .addEventListener("click", async function (event) {
     const targetElement = event.target;
-    if (targetElement.closest('.activity-tab')) {
-        const type = targetElement.textContent.toLowerCase();
+    if (targetElement.closest(".activity-tab")) {
+      const type = targetElement.textContent.toLowerCase();
 
-        resetTab(targetElement);
-        if (type === 'recent') {
-            createParentList([`${type}-list`, "common-list", `row-list`]);
-        } else {
-            createParentList([`${type}-list`, "common-list", `column-list`]);
-        }
+      resetTab(targetElement);
+      if (type === "recent") {
+        createParentList([`${type}-list`, "common-list", `row-list`]);
+      } else {
+        createParentList([`${type}-list`, "common-list", `column-list`]);
+      }
 
-        fetchDataByType(type);
+      fetchDataByType(type);
     }
-});
+  });
 
 async function fetchDataByType(type) {
-    let data = null;
-    let callback = null;
+  let data = null;
+  let callback = null;
 
-    switch (type) {
-        case 'recent':
-            data = await fetchRecent();
-            callback = processRowData;
-            break;
-        case 'like':
-            data = await fetchLike();
-            callback = processColumnData;
-            break;
-        default:
-            throw new Error('이상한 타입');
-    }
+  switch (type) {
+    case "recent":
+      data = await fetchRecent();
+      callback = processRowData;
+      break;
+    case "like":
+      data = await fetchLike();
+      callback = processColumnData;
+      break;
+    default:
+      throw new Error("이상한 타입");
+  }
 
-    callback(data, type);
+  callback(data, type);
 }
-
 
 const processRowData = (results, type) => {
-    results.forEach((data) => {
-        renderRowList(data, type);
-    })
-}
+  results.forEach((data) => {
+    renderRowList(data, type);
+  });
+};
 
 const processColumnData = (results, type) => {
-    results.forEach((data) => {
-        renderColumnList(data, type);
-    })
-}
-
+  results.forEach((data) => {
+    renderColumnList(data, type);
+  });
+};
 
 function createParentList(classList) {
-    parentList = document.createElement('ul');
+  parentList = document.createElement("ul");
 
-    classList.forEach((className) => {
-        parentList.classList.add(className);
-    });
+  classList.forEach((className) => {
+    parentList.classList.add(className);
+  });
 
-    activity.appendChild(parentList);
+  activity.appendChild(parentList);
 }
 
 function renderRowList(data, type) {
-    const htmlContent = `
+  const htmlContent = `
         <li class="${type}-container row-container">
             <div class="${type}-img row-img">
-                <a href="/view/detail.html?id=${data.id}"> <img src="${data.backdrop_path}" alt="${data.title}"></a>
+                <a href="../view/detail.html?id=${data.id}"> <img src="${data.backdrop_path}" alt="${data.title}"></a>
             </div>
             <div class="${type}-title row-title">
                 <p>${data.title}</p>
@@ -136,14 +138,18 @@ function renderRowList(data, type) {
         </li>
     `;
 
-    parentList.innerHTML += htmlContent;
+  parentList.innerHTML += htmlContent;
 }
 
 function renderColumnList(data, type) {
-    let htmlContent = `
+  let htmlContent = `
             <li class="${type} column-container">
                 <div class="${type} column-img">
-                    <a href="/view/detail.html?id=${data.movie_id}"><img src="https://image.tmdb.org/t/p/w500/${data.movie_img}"></a>
+                    <a href="../view/detail.html?id=${
+                      data.movie_id
+                    }"><img src="https://image.tmdb.org/t/p/w500/${
+    data.movie_img
+  }"></a>
                 </div>
                 <div class="${type} column-contents">
                     <h4>${data.movie_title}</h4>
@@ -156,56 +162,52 @@ function renderColumnList(data, type) {
                     <p>${handleTimeCalculate(data.movie_like_time)}</p>
                 </div>
             </li>
-        `
+        `;
 
-    parentList.innerHTML += htmlContent;
+  parentList.innerHTML += htmlContent;
 }
-
 
 function handleTimeCalculate(time) {
-    const getDate = new Date(time);
-    const nowDate = new Date();
+  const getDate = new Date(time);
+  const nowDate = new Date();
 
-    const CalculateDate = nowDate - getDate;
-    const calcSeconds = Math.floor(CalculateDate / 1000);
-    const calcMinutes = Math.floor(calcSeconds / 60);
-    const calcHours = Math.floor(calcMinutes / 60);
-    const calcDays = Math.floor(calcHours / 24);
+  const CalculateDate = nowDate - getDate;
+  const calcSeconds = Math.floor(CalculateDate / 1000);
+  const calcMinutes = Math.floor(calcSeconds / 60);
+  const calcHours = Math.floor(calcMinutes / 60);
+  const calcDays = Math.floor(calcHours / 24);
 
-    if (calcSeconds < 60) {
-        return '방금';
-    } else if (calcMinutes < 60) {
-        return `${calcMinutes}분 전`;
-    } else if (calcHours < 24) {
-        return `${calcHours}시간 전`;
-    }
-    return `${calcDays}일 전`;
+  if (calcSeconds < 60) {
+    return "방금";
+  } else if (calcMinutes < 60) {
+    return `${calcMinutes}분 전`;
+  } else if (calcHours < 24) {
+    return `${calcHours}시간 전`;
+  }
+  return `${calcDays}일 전`;
 }
 
-
 (function () {
-    const session = sessionStorage.getItem("loginState") === "true";
+  const session = sessionStorage.getItem("loginState") === "true";
 
-    if (session) {
-        const ulElement = document.querySelector('ul.activity-header-type');
+  if (session) {
+    const ulElement = document.querySelector("ul.activity-header-type");
 
-        const likeBox = document.createElement('li');
-        likeBox.className = 'activity-tab like-box';
-        likeBox.innerHTML = '<p>Like</p>';
-        const firstChild = ulElement.firstChild;
-        ulElement.insertBefore(likeBox, firstChild);
+    const likeBox = document.createElement("li");
+    likeBox.className = "activity-tab like-box";
+    likeBox.innerHTML = "<p>Like</p>";
+    const firstChild = ulElement.firstChild;
+    ulElement.insertBefore(likeBox, firstChild);
 
-        likeBox.classList.add('selected')
+    likeBox.classList.add("selected");
 
-        createParentList([`like-list`, "common-list", `column-list`]);
-        fetchDataByType('like');
+    createParentList([`like-list`, "common-list", `column-list`]);
+    fetchDataByType("like");
+  } else {
+    const recentBox = document.querySelector(".recent-box p");
+    recentBox.classList.add("selected");
 
-    } else {
-        const recentBox = document.querySelector('.recent-box p');
-        recentBox.classList.add('selected')
-
-        createParentList([`recent-list`, "common-list", `row-list`]);
-        fetchDataByType('recent');
-    }
-
-})()
+    createParentList([`recent-list`, "common-list", `row-list`]);
+    fetchDataByType("recent");
+  }
+})();
